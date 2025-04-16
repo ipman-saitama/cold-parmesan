4f9998e2c76ebea33573939ad7e8633c59aedf45
ipman-saitama <adriancwip@hotmail.com>
2025-04-16 22:13:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
 import streamlit as st
-import pandas as pd
-import requests
 from optimizer import generate_best_team
 
 def main():
     st.title("Fantasy Premier League Optimizer")
     
-    # Display the best team
+    # Fetch and display the best team
     team = generate_best_team()
     
     st.header("Your Best Fantasy Premier League Team")
-    st.write(team)
-    
+    st.dataframe(team)
+
 if __name__ == "__main__":
     main()
